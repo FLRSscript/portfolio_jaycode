bb821916330ed27831554ab4dd871b7a3a2ad430
FLRSscript <flrsscript@users.noreply.github.com>
2023-08-25 13:43:46 -0600

    act

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 
 const listaProyectos = () => fetch('https://raw.githubusercontent.com/FLRSscript/proyectosAPI/main/db.json').then(respuesta => respuesta.json());
 
-const mostrarProyecto = (nombre, imagen, descripcion, tecnologias) => {
+// Función para redireccionar a una página con el ID como parámetro
+const redirigirProyecto = (id, nombre, imagen, descripcion, tecnologias, info, link) => {
+    const urlParams = new URLSearchParams();
+    urlParams.append('id', id);
+    urlParams.append('nombre', nombre);
+    urlParams.append('imagen', imagen);
+    urlParams.append('descripcion', descripcion);
+    urlParams.append('tecnologias', tecnologias);
+    urlParams.append('info', info);
+    urlParams.append('link', link);
+
+    // Redirigir a la página con los parámetros en la URL
+    window.location.href = `proyecto.html?${urlParams.toString()}`;
+};
+
+// Función para mostrar el proyecto con enlace y capturar el ID
+const mostrarProyecto = (id, nombre, imagen, descripcion, tecnologias, info, link) => {
     const proyectoCard = document.createElement('div');
     proyectoCard.className = 'proyecto__card';
 
@@ -25,7 +41,7 @@ const mostrarProyecto = (nombre, imagen, descripcion, tecnologias) => {
         </div><!-- PROYECTO TECNOLOGIAS -->
         <div class="proyecto__links">
             <div class="link__git">
-                <a class="github" href=""><i class="bi bi-info-circle-fill"></i></a>
+                <a id="${id}" class="github" href="javascript:void(0);"><i class="bi bi-info-circle-fill"></i></a>
             </div>
             <div class="link__link">
                 <a class="link" href=""><i class="bi bi-link"></i></a>
@@ -35,15 +51,52 @@ const mostrarProyecto = (nombre, imagen, descripcion, tecnologias) => {
     `;
 
     proyectoCard.innerHTML = contenido;
+
+    // Agregar evento de clic al enlace con ID
+    const enlaceProyecto = proyectoCard.querySelector('.github');
+    enlaceProyecto.addEventListener('click', () => {
+        redirigirProyecto(id, nombre, imagen, descripcion, tecnologias, info, link);
+    });
+
     return proyectoCard;
 };
 
 listaProyectos().then(data => {
-    data.forEach(({ nombre, imagen, descripcion, tecnologias }) => {
+    data.forEach(({id, nombre, imagen, descripcion, tecnologias, info, link }) => {
+        
         const contenedorProyectos = document.querySelector('.proyectos__contenedor');
-        const proyecto = mostrarProyecto(nombre, imagen, descripcion, tecnologias);
+        const proyecto = mostrarProyecto(id, nombre, imagen, descripcion, tecnologias, info, link);
 
         contenedorProyectos.appendChild(proyecto);
 
     })
 })
+
+        // // Obtener los parámetros de la URL
+        //         const urlParams = new URLSearchParams(window.location.search);
+        //         const id = urlParams.get('id');
+        //         console.log(id);
+        //         const nombre = urlParams.get('nombre');
+        //         const imagen = urlParams.get('imagen');
+        //         const descripcion = urlParams.get('descripcion');
+
+        //         // Mostrar las propiedades del proyecto en el div
+        //         const proyectoDetalleDiv = document.querySelector('.proyecto__detalle');
+        //         proyectoDetalleDiv.innerHTML = `
+        //         <div class="proyecto__banner" style="background-Image: url(${imagen})"></div>
+        //         <div class="proyecto__info">
+        //             <h4 class="proyecto__nombre">${nombre}</h4>
+        //             <p class="proyecto__detalles">${descripcion}</p>
+        //             <div class="proyecto__tecnologias">
+                        
+        //             </div><!-- PROYECTO TECNOLOGIAS -->
+        //             <div class="proyecto__links">
+        //                 <div class="link__git">
+        //                     <a id="${id}" class="github" href="javascript:void(0);"><i class="bi bi-info-circle-fill"></i></a>
+        //                 </div>
+        //              <div class="link__link">
+        //                     <a class="link" href=""><i class="bi bi-link"></i></a>
+        //                 </div>
+        //             </div><!-- PROYECTO LINKS -->
+        //         </div><!-- PROYECTO INFO -->
+        //         `;
